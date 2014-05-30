611fa19731583796191924b061555cb51d5b40eb
Ryan Casey <ryepdx@gmail.com>
2014-05-30 11:08:10 -0700

    Add account_status and get_quickship_id functions to res.user model.

--- res_users.py
+++ res_users.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+import time
+import openerp.addons.web.http as http
+from ..shipping_api_usps.api import v1 as usps_api
 from openerp.osv import fields,osv
 
 class res_users(osv.osv):
@@ -10,5 +14,18 @@ class res_users(osv.osv):
     }
 
     _sql_constraints = [('quickship_id', 'unique(quickship_id)', 'A user with that Quickship ID already exists.')]
+
+    def account_status(self, cr, uid, test=None):
+        '''Get the user's current account info..'''
+        config = usps_api.get_config(cr, uid)
+        response = usps_api.get_account_status(config, test=(test if test != None else config.sandbox))
+
+        if hasattr(response, 'postage_balance'):
+            return {'success': True, 'postage_balance': response.postage_balance}
+
+        return {'success': False, 'error': response.message}
+
+    def get_quickship_id(self, cr, uid, user_id=None):
+        return self.pool.get("res.users").browse(cr, uid, user_id or uid).quickship_id
 
 res_users()
